009a9b51f91acc3bba266c710ca2343bc9864e32
Devanshukoli <kolidevanshu02@gmail.com>
2025-05-14 22:59:33 +0530

    add validation to form

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-let btn = $('#button');
+function validate() {
+    var name = document.getElementById('your-name').value
+    var email = document.getElementById('email').value
+    var message = document.getElementById('message').value
+    const errorMessage = document.getElementById("errorMessage"); 
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
-        btn.addClass('show');
-    } else {
-        btn.removeClass('show');
+    errorMessage.style.padding = "12px";
+
+    let text;
+    if (name.length < 0) {
+        text = "Please Enter Name."
+        errorMessage.innerHTML = text;
+        return;
     }
-});
 
-btn.on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: 0 }, '300');
-});
+    if (email.indexOf("@") == -1 || email.length < 0) {
+        text = "Please Enter Valid Email";
+        errorMessage.innerHTML = text;
+        return
+    }
+
+    if (message.length <= 0) {
+        text = "Pleaes Enter Message";
+        errorMessage.innerHTML = text;
+        return;
+    }
+
+    alert("Form Submitted Successfully!")
+    return;
+}
