e8142b7f07e932fd67103629a299661b7c30690d
Devanshukoli <kolidevanshu02@gmail.com>
2025-06-03 22:12:11 +0530

    add func. for makrdown

--- utils/markdown.js
+++ utils/markdown.js
@@ -18,27 +18,32 @@ export function getAllPosts() {
     const posts = fileNames.map(fileName => {
         // Remove ".md" from file name to get slug
         const slug = fileName.replace(/\.md$/, '');
-        
+
         // Read markdown file as string
         const fullPath = path.join(postsDirectory, fileName);
         const fileContents = fs.readFileSync(fullPath, 'utf8');
-        
+
         // Use gray-matter to parse the post metadata section
         const { data: frontmatter, content } = matter(fileContents);
-        
-        // Convert markdown to HTML
+
+        // Convert markdown into HTML string
         const htmlContent = marked(content);
-        
+
+        // Combine the data
         return {
             slug,
             frontmatter,
             content: htmlContent
         };
     });
-    
+
     // Sort posts by date
     return posts.sort((a, b) => {
-        return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
+        if (a.frontmatter.date < b.frontmatter.date) {
+            return 1;
+        } else {
+            return -1;
+        }
     });
 }
 
@@ -46,10 +51,9 @@ export function getAllPosts() {
 export function getPostBySlug(slug) {
     const fullPath = path.join(process.cwd(), 'content/blog', `${slug}.md`);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
-    
     const { data: frontmatter, content } = matter(fileContents);
     const htmlContent = marked(content);
-    
+
     return {
         slug,
         frontmatter,
