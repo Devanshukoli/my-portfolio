a67cb371ece659d55b2ac0dd93c6ef835bf27ba2
Devanshukoli <kolidevanshu02@gmail.com>
2025-06-25 23:19:15 +0530

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,39 @@
+import AboutView from "@/view/AboutView.vue";
+import BlogView from "@/view/BlogView.vue";
+import ContactView from "@/view/ContactView.vue";
+import HomeView from "@/view/HomeView.vue";
+import ProjectView from "@/view/ProjectView.vue";
 import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: BlogView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
+    },
+    {
+      path: '/project',
+      name: 'project',
+      component: ProjectView
+    }
+  ]
+})
+
+export default router;
