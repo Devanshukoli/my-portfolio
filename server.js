5ccbd5c226ee799d6543b30ea47d8d78b632eafe
Devanshukoli <kolidevanshu02@gmail.com>
2026-01-07 22:46:31 +0530

    fix: typo

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const contactLimit = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minute
   max: 5, // limit each IP to 3 request per windowMs
   message: { error: 'Too many requests, Please try again later.' }
-})
+});
 
 const window = new JSDOM('').window;
 const DOMPurify = createDOMPurifier(window)
