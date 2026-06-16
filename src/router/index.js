5c006828b15fa1d8e7c9006aacd7a9626fec5ae2
Devanshukoli <kolidevanshu02@gmail.com>
2026-06-16 16:00:06 +0530

    refactor: remove blog and tracker views, update header links

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,7 @@
 import AboutView from "@/view/AboutView.vue";
-import BlogList from "@/view/BlogList.vue";
-import BlogPost from "@/view/BlogPost.vue";
 import ContactView from "@/view/ContactView.vue";
 import HomeView from "@/view/HomeView.vue";
 import ProjectView from "@/view/ProjectView.vue";
-import TrackerView from "@/view/TrackerView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
@@ -21,17 +18,6 @@ const router = createRouter({
       component: AboutView
     },
     {
-      path: '/blog',
-      name: 'blog',
-      component: BlogList
-    },
-    {
-      path: '/blog/:slug',
-      name: 'blog-post',
-      component: BlogPost,
-      props: true
-    },
-    {
       path: '/contact',
       name: 'contact',
       component: ContactView
@@ -41,11 +27,6 @@ const router = createRouter({
       name: 'project',
       component: ProjectView
     },
-    {
-      path: '/tracker',
-      name: 'tracker',
-      component: TrackerView
-    }
   ]
 })
 
